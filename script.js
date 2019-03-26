f4a1cfef01cef965179a21de50134cf554f550d0
Шафоростов Андрей <andpr99@gmail.com>
2019-03-26 23:32:24 +0200

    init bluetooth

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 jQuery(function($) {
-
+  navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+  .then(function(device) {
+    // Step 2: Connect to it
+    console.log(device);
+  });
 });
