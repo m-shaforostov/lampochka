c2bb8febe3e6ec04c0c51bafb264d415974361cf
Шафоростов Андрей <andpr99@gmail.com>
2019-03-26 23:36:08 +0200

    timeout and error

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 jQuery(function($) {
-  navigator.bluetooth.requestDevice({ acceptAllDevices: true })
-  .then(function(device) {
-    // Step 2: Connect to it
-    console.log(device);
-  });
+  setTimeout(function() {
+    navigator.bluetooth.requestDevice({ acceptAllDevices: true })
+    .then(function(device) {
+      // Step 2: Connect to it
+      console.log(device);
+    })
+    .catch(function(error) {
+      console.error(error);
+    })
+  }, 2000)
 });
